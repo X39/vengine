b305977fda2982648689494f3beff41416e1ee2e
X39 <X39@users.noreply.github.com>
2021-09-08 23:54:40 +0200

    prepwork

---
+++ vengine/scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marco.silipo on 08.09.2021.
+//
+
+#include "scene.hpp"
---
+++ vengine/scene.hpp
@@ -0,0 +1,22 @@
+//
+// Created by marco.silipo on 08.09.2021.
+//
+
+#ifndef GAME_PROJ_SCENE_HPP
+#define GAME_PROJ_SCENE_HPP
+
+
+namespace vengine
+{
+    class scene
+    {
+    public:
+        scene() {}
+        ~scene() {}
+
+
+    };
+}
+
+
+#endif //GAME_PROJ_SCENE_HPP
